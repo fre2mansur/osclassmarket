555d7db46317944de44284bd086a5f246d06bd1a
WEBmods <lil.funk.133@gmail.com>
2019-08-17 23:27:05 +0200

    Formatting

--- item.php
+++ item.php
@@ -40,33 +40,36 @@ osc_current_web_theme_path('header.php');
         	<div class="col-md-8">
             	<div class="content">
                 	<h2><?php echo osc_item_title();?></h2>
-            	        <div class="row col-12">
-                            <?php if(osc_count_item_resources() > 0) { ?>
-                                <h4 class="mt-3">Screenshots</h4>
-                                <div class="carousel slide mb-3" id="gallery" data-ride="carousel" data-interval="false">
-                                    <ol class="carousel-indicators">
-                                        <?php for($i = 0; $i < osc_count_item_resources(); $i++) { ?>
-                                            <li data-target="#gallery" data-slide-to="<?php echo $i; ?>"<?php if($i === 0) { echo ' class="active"'; } ?>></li>
-                                        <?php } ?>
-                                        </ol>
-                                    <div class="sgallery carousel-inner text-center">
-                                        <?php for($i = 1; osc_has_item_resources(); $i++) { ?>
-                                            <?php $title = $i.'/'.osc_count_item_resources().' - '.osc_item_title(); ?>
-                                            <div class="carousel-item<?php if($i === 1) { echo ' active'; } ?>">
-                                                <a href="<?php echo osc_resource_url(); ?>" ><img src="<?php echo osc_resource_url(); ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>" class="img-fluid"/></a>
-                                            </div>
-                                        <?php } ?>
-                                    </div>
-                                    <a class="carousel-control-prev" href="#gallery" data-slide="prev">
-                                    <span class="carousel-control-prev-icon"></span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#gallery" data-slide="next">
-                                    <span class="carousel-control-next-icon"></span>
-                                    </a>
+        	        <div class="row col-12">
+                        <?php if(osc_count_item_resources() > 0) { ?>
+                            <h4 class="mt-3">Screenshots</h4>
+                            <div class="carousel slide mb-3" id="gallery" data-ride="carousel" data-interval="false">
+                                <ol class="carousel-indicators">
+                                    <?php for($i = 0; $i < osc_count_item_resources(); $i++) { ?>
+                                        <li data-target="#gallery" data-slide-to="<?php echo $i; ?>"<?php if($i === 0) { echo ' class="active"'; } ?>></li>
+                                    <?php } ?>
+                                    </ol>
+                                <div class="sgallery carousel-inner text-center">
+                                    <?php for($i = 1; osc_has_item_resources(); $i++) { ?>
+                                        <?php $title = $i.'/'.osc_count_item_resources().' - '.osc_item_title(); ?>
+                                        <div class="carousel-item<?php if($i === 1) { echo ' active'; } ?>">
+                                            <a href="<?php echo osc_resource_url(); ?>" ><img src="<?php echo osc_resource_url(); ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>" class="img-fluid"/></a>
+                                        </div>
+                                    <?php } ?>
                                 </div>
-                            <?php } ?>
-                        </div>
-              		<?php echo osc_item_description(); ?>
+                                <a class="carousel-control-prev" href="#gallery" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                                </a>
+                                <a class="carousel-control-next" href="#gallery" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div class="row col-12 d-block">
+                        <h4 class="mt-3">Description</h4>
+                        <p><?php echo osc_item_description(); ?></p>
+                    </div>
                     <br /><br />
                     <?php osc_run_hook('list_questions');?>
                     <?php echo osc_run_hook('questions_comment'); ?>
@@ -79,16 +82,16 @@ osc_current_web_theme_path('header.php');
                 		<?php //echo show_avatar(osc_item_user_id()); ?>
                     </div>
                     <div class="author">
-                     <h5><strong><a class="text-dark" href="<?php echo osc_user_public_profile_url();?>"><?php echo osc_item_contact_name();?></a></strong> </h5>
-                      <?php if(osc_is_admin_user_logged_in()){ ?>
-                      <h5><small><?php echo user_total_plugins(osc_item_user_id()); ?> plugins | <?php echo user_total_themes(osc_item_user_id()); ?> Themes  <?php if(osc_is_admin_user_logged_in()){ ?>| 140 Posts <?php } ?></small></h5>
-                      <p><button class="btn btn-danger btn-sm">Follow</button> 120 followers <br /></p>
-                      <button class="btn btn-success">Job offer</button>
-                      <?php } ?>
-                 </div>
+                        <h5><strong><a class="text-dark" href="<?php echo osc_user_public_profile_url();?>"><?php echo osc_item_contact_name();?></a></strong> </h5>
+                        <?php if(osc_is_admin_user_logged_in()) { ?>
+                            <h5><small><?php echo user_total_plugins(osc_item_user_id()); ?> plugins | <?php echo user_total_themes(osc_item_user_id()); ?> themes  <?php if(osc_is_admin_user_logged_in()){ ?>| 140 posts <?php } ?></small></h5>
+                            <p><button class="btn btn-danger btn-sm">Follow</button> 120 followers <br /></p>
+                            <button class="btn btn-success">Job offer</button>
+                        <?php } ?>
+                    </div>
 
                  <?php if(osc_get_preference('donation_link', 'donation_link_'.osc_item_user_id()) !=''){ ?>
-                 <a target="_blank" href="<?php echo osc_get_preference('donation_link', 'donation_link_'.osc_item_user_id()); ?>" class="btn btn-primary mb-1">Donate</a>
+                     <a target="_blank" href="<?php echo osc_get_preference('donation_link', 'donation_link_'.osc_item_user_id()); ?>" class="btn btn-primary mb-1">Donate</a>
                  <?php } ?>
             </div>
 

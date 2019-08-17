13973ea665e3d5d5d415bc81da411f1d56467bbc
WEBmods <lil.funk.133@gmail.com>
2019-08-17 22:36:17 +0200

    Formatting changes.

--- main.php
+++ main.php
@@ -2,14 +2,16 @@
 <?php osc_current_web_theme_path('header.php'); ?>
 <section id="search-header">
     <div class="jumbotron jumbotron-fluid text-center bg-info text-white mb-0">
-      <div class="container">
-        <h1>Osclass Community</h1>
-        <p>Free osclass plugins and themes</p>
-        <a href="https://github.com/navjottomer/Osclass/archive/master.zip" class="btn btn-lg btn-dark">Download Osclass</a><br>
-         (Community Edition)
-      </div>
+        <div class="container">
+            <h1>Osclass Community</h1>
+            <p>Free Osclass plugins and themes</p>
+            <a href="https://github.com/navjottomer/Osclass/archive/master.zip" class="btn btn-lg btn-dark">Download Osclass</a>
+            <p class="mt-2">(Community Edition)</p>
+        </div>
     </div>
 </section>
+
+
 <!--<section id="top-downloads">
      <div class="container">
      	<h2 class="text-center">Top Downloads</h2>
@@ -21,7 +23,7 @@
                   		<img alt="Card image cap" src="https://market.osclass.org/oc-content/themes/osclass_org/images/no_photo_plugin_square.gif" class="card-img-top">
                     </div>
                     <div class="col-9">
-                       <h5><b><a class="text-dark" href="">Avatar plugin</a></b> <br /><small>by: <a href="">Media.dmj</a></small></h5>
+                       <h5><strong><a class="text-dark" href="">Avatar plugin</a></strong> <br /><small>by: <a href="">Media.dmj</a></small></h5>
                       <p class="mb-0">Avatar plugin can upload the pro...</p>
                       <p class="mb-0 text-secondary"><small>120 views | 2028 Downloads</small></p>
                     </div>
@@ -34,7 +36,7 @@
                   		<img alt="Card image cap" src="https://market.osclass.org/oc-content/themes/osclass_org/images/no_photo_plugin_square.gif" class="card-img-top">
                     </div>
                     <div class="col-9">
-                       <h5><b><a class="text-dark" href="">Avatar plugin</a></b> <br /><small>by: <a href="">Media.dmj</a></small></h5>
+                       <h5><strong><a class="text-dark" href="">Avatar plugin</a></strong> <br /><small>by: <a href="">Media.dmj</a></small></h5>
                       <p class="mb-0">Avatar plugin can upload the pro...</p>
                       <p class="mb-0 text-secondary"><small>120 views | 2028 Downloads</small></p>
                     </div>
@@ -47,7 +49,7 @@
                   		<img alt="Card image cap" src="https://market.osclass.org/oc-content/themes/osclass_org/images/no_photo_plugin_square.gif" class="card-img-top">
                     </div>
                     <div class="col-9">
-                       <h5><b><a class="text-dark" href="">Avatar plugin</a></b> <br /><small>by: <a href="">Media.dmj</a></small></h5>
+                       <h5><strong><a class="text-dark" href="">Avatar plugin</a></strong> <br /><small>by: <a href="">Media.dmj</a></small></h5>
                       <p class="mb-0">Avatar plugin can upload the pro...</p>
                       <p class="mb-0 text-secondary"><small>120 views | 2028 Downloads</small></p>
                     </div>
@@ -60,7 +62,7 @@
                   		<img alt="Card image cap" src="https://market.osclass.org/oc-content/themes/osclass_org/images/no_photo_plugin_square.gif" class="card-img-top">
                     </div>
                     <div class="col-9">
-                       <h5><b><a class="text-dark" href="">Avatar plugin</a></b> <br /><small>by: <a href="">Media.dmj</a></small></h5>
+                       <h5><strong><a class="text-dark" href="">Avatar plugin</a></strong> <br /><small>by: <a href="">Media.dmj</a></small></h5>
                       <p class="mb-0">Avatar plugin can upload the pro...</p>
                       <p class="mb-0 text-secondary"><small>120 views | 2028 Downloads</small></p>
                     </div>
@@ -74,31 +76,30 @@
      </div>
 </section>-->
 
-
-<section id="top-uploads" style="background: #f4f4f4;" class="pt-4 pb-4">
+<section id="recent-uploads" class="pt-4 pb-4 gray-bg">
     <div class="container">
-     	<h2 class="text-center">Recently Uploaded</h2>
-        <br />
-
+     	<h2 class="text-center mb-4">Recently Uploaded</h2>
         <div class="row">
-            <?php while ( osc_has_latest_items() ) { ?>
-            <div class="col-md-6">
-              	<div class="item">
-                	<div class="col-3 float-left">
-                        <img alt="<?php echo osc_item_title(); ?>" src="<?php echo item_default_image_url(); ?>" class="card-img-top">
+            <?php while(osc_has_latest_items()) { ?>
+                <div class="col-md-6">
+                  	<div class="item">
+                    	<div class="col-3 float-left">
+                            <img alt="<?php echo osc_item_title(); ?>" src="<?php echo item_default_image_url(); ?>" class="card-img-top">
+                        </div>
+                        <div class="col-9">
+                            <h5 class="mb-0"><strong><a class="text-dark" href="<?php echo osc_item_url();?>"><?php echo osc_item_title(); ?></a></strong></h5>
+                            <p class="mb-1">by: <a href="<?php echo osc_user_public_profile_url(osc_item_user_id());?>"><?php echo osc_item_contact_name();?></a></p>
+                            <p class="mb-0"><?php echo osc_highlight( strip_tags( osc_item_description()), 25); ?></p>
+                            <p class="mb-0 text-secondary"><small><?php echo osc_item_views(); ?> views</small></p>
+                        </div>
+                        <hr />
                     </div>
-                    <div class="col-9">
-                        <h5><b><a class="text-dark" href="<?php echo osc_item_url();?>"><?php echo osc_item_title(); ?></a></b> <br /><small>by: <a href="<?php echo osc_user_public_profile_url(osc_item_user_id());?>"><?php echo osc_item_contact_name();?></a></small></h5>
-                        <p class="mb-0"><?php echo osc_highlight( strip_tags( osc_item_description()), 25) ; ?></p>
-                        <p class="mb-0 text-secondary"><small><?php echo osc_item_views(); ?> views</small></p>
-                    </div>
-                    <hr />
                 </div>
-            </div>
             <?php } ?>
         </div>
     </div>
 </section>
+
 <?php if(osc_is_admin_user_logged_in()){ ?> <!--Need to remove this condition later-->
 <section id="top-developers" class="pt-4 pb-4">
 	<div class="container">
@@ -109,7 +110,7 @@
                 <img width="120" alt="Card image cap" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="rounded-circle">
             </div>
             <div class="">
-                <h5><b><a class="text-dark" href="">Media.dmj</a></b> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
+                <h5><strong><a class="text-dark" href="">Media.dmj</a></strong> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
                  <p class="mb-0 text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
             </div>
          </div>
@@ -118,7 +119,7 @@
                 <img width="120" alt="Card image cap" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="rounded-circle">
             </div>
             <div class="">
-                <h5><b><a class="text-dark" href="">Media.dmj</a></b> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
+                <h5><strong><a class="text-dark" href="">Media.dmj</a></strong> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
                  <p class="mb-0 text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
             </div>
          </div>
@@ -127,7 +128,7 @@
                 <img width="120" alt="Card image cap" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="rounded-circle">
             </div>
             <div class="">
-                <h5><b><a class="text-dark" href="">Media.dmj</a></b> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
+                <h5><strong><a class="text-dark" href="">Media.dmj</a></strong> <br /><small>5 plugins | 8 Themes | 140 Posts</small></h5>
                  <p class="mb-0 text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
             </div>
          </div>

dbc096b1362d8ea7abbbb3f62e545e24ea078a9b
WEBmods <lil.funk.133@gmail.com>
2019-09-01 23:53:36 +0200

    Added DAO class - fix

--- classes/ModelMarket.php
+++ classes/ModelMarket.php
@@ -1,10 +1,4 @@
 <?php
-/* Developed by WEBmods
- * Zagorski oglasnik j.d.o.o. za usluge | www.zagorski-oglasnik.com
- *
- * License: GPL-3.0-or-later
- * More info in license.txt
-*/
 if(!defined('ABS_PATH')) exit('ABS_PATH is not loaded. Direct access is not allowed.');
 
 class MarketModel extends DAO {
